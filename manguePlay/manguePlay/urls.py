4f1c40a0f16e9c89c1eb3f7b476d0b197a0c7b83
Igor Couto <iacncouto@hotmail.com>
2024-10-14 11:36:37 -0300

    master-update

--- manguePlay/manguePlay/urls.py
+++ manguePlay/manguePlay/urls.py
@@ -19,7 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('usuarios.urls')),
+    path('local_doacao/',include('local_doacao.urls')),
 ]
