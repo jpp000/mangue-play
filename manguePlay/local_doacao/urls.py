8ca4efb09403dafc83fa59d79c42c130ebabc6a1
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-10-14 14:28:36 -0300

    update

--- manguePlay/local_doacao/urls.py
+++ manguePlay/local_doacao/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('local', views.local_doacao, name='local_doacao'),  
+    path('adicionar_doacao/', views.adicionar_doacao, name='adicionar_doacao'),
+    path('usuario/', views.adicionar_doacao, name='user_dashboard'),   # Nova URL para salvar a doação
 ]
--- manguePlay/local_doacao/views.py
+++ manguePlay/local_doacao/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Doacao
 
-# Create your views here.
 def local_doacao(request):
+    # Renderiza apenas o formulário
+    return render(request, 'local.html')
+
+def adicionar_doacao(request):
+    if request.method == 'POST':
+        nome_doador = request.POST.get('nome_doador')
+        contato = request.POST.get('contato')
+        local = request.POST.get('local')
+        data = request.POST.get('data')
+        hora_inicio = request.POST.get('hora_inicio')
+        hora_fim = request.POST.get('hora_fim')
+        observacoes = request.POST.get('observacoes')
+
+        # Criar uma nova instância e salvar no banco de dados
+        doacao = Doacao(
+            nome_doador=nome_doador,
+            contato=contato,
+            local=local,
+            data=data,
+            hora_inicio=hora_inicio,
+            hora_fim=hora_fim,
+            observacoes=observacoes
+        )
+        doacao.save()
+
+        # Redirecionar para a página inicial ou de confirmação
+        return redirect('home')  # Altere 'home' para o nome da view que você deseja redirecionar após salvar
+
     return render(request, 'local.html')
