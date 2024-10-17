38506b98c8f46e0555c3e02f9f99c196d334c00c
Igor Couto <iacncouto@hotmail.com>
2024-10-17 18:07:26 -0300

    update-absurdo

--- manguePlay/brinquedos/urls.py
+++ manguePlay/brinquedos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('adicionar_brinquedo/', views.adicionar_brinquedo, name='adicionar_brinquedo'),
-  path('visualizar_brinquedos/', views.visualizar_brinquedos, name='visualizar_brinquedos')
+  path('visualizar_brinquedos/', views.visualizar_brinquedos, name='visualizar_brinquedos'),
+  path('visualizar_brinquedos_admin/', views.visualizar_brinquedos_admin, name='visualizar_brinquedos_admin')
 ]
