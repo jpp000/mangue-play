f55bf5cc8e4239f40dd2542a69daf5d8907b6f0e
gustavocarneiro453 <gustavocidc@gmail.com>
2024-11-12 15:17:33 -0300

    concluida(A pagina de login esta com erro de redirecionamento)

--- manguePlay/fornecedor/urls.py
+++ manguePlay/fornecedor/urls.py
@@ -1,7 +1,11 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('adicionar/', views.adicionar_fornecedor, name='adicionar_fornecedor'),
     path('listar/', views.listar_fornecedores, name='listar_fornecedores'),
+    path('editar/<int:pk>/', views.editar_fornecedor, name='editar_fornecedor'),
+    path('excluir/<int:pk>/', views.excluir_fornecedor, name='excluir_fornecedor'),
+    path('login/', views.login_view, name='login'),  # Defina uma URL de login
 ]
