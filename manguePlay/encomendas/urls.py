da3ff6940517612ebdb1ddc57cc9493e740253c6
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-11-12 21:07:30 -0300

    atualização

--- manguePlay/encomendas/urls.py
+++ manguePlay/encomendas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('adicionar/', views.adicionar_encomenda, name='adicionar_encomenda'),
     path('visualizar/', views.visualizar_encomendas, name='visualizar_encomendas'),
+    path('excluir/<int:encomenda_id>/', views.excluir_encomenda, name='excluir_encomenda'),
+    path('editar_encomenda/<int:id>/', views.editar_encomenda, name='editar_encomenda'),
 ]
