56e82a040801ffd6bf6181b02e5d3fca7e49a4be
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-11-12 21:34:15 -0300

    atualização

--- manguePlay/fornecedor/urls.py
+++ manguePlay/fornecedor/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('adicionar/', views.adicionar_fornecedor, name='adicionar_fornecedor'),
     path('listar/', views.listar_fornecedores, name='listar_fornecedores'),
+    path('editar/<int:pk>/', views.editar_fornecedor, name='editar_fornecedor'),
+    path('excluir/<int:pk>/', views.excluir_fornecedor, name='excluir_fornecedor'),
 ]
--- manguePlay/fornecedor/views.py
+++ manguePlay/fornecedor/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Fornecedor
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
+
 
 # View para adicionar fornecedor
 def adicionar_fornecedor(request):
@@ -25,7 +28,34 @@ def adicionar_fornecedor(request):
 
     return render(request, 'fornecedor.html')  # Renderiza o formulário para adicionar fornecedor
 
-# View para listar fornecedores
+
 def listar_fornecedores(request):
-    fornecedores = Fornecedor.objects.all()  # Recupera todos os fornecedores do banco de dados
-    return render(request, 'visualizar_forne.html', {'fornecedores': fornecedores})  # Renderiza a página de listagem
+    fornecedores = Fornecedor.objects.all()
+    return render(request, 'visualizar_forne.html', {'fornecedores': fornecedores})
+
+def editar_fornecedor(request, pk):
+    fornecedor = get_object_or_404(Fornecedor, pk=pk)
+
+    if request.method == "POST":
+        fornecedor.nome = request.POST['nome']
+        fornecedor.contato_numero = request.POST['contato_numero']
+        fornecedor.frequencia_de_entrega = request.POST['frequencia_de_entrega']
+        fornecedor.endereco = request.POST['endereco']
+        fornecedor.email = request.POST['email']
+        fornecedor.save()
+
+        messages.success(request, "Fornecedor atualizado com sucesso!")
+        return redirect('listar_fornecedores')
+
+    return render(request, 'editar_fornecedor.html', {'fornecedor': fornecedor})
+
+def excluir_fornecedor(request, pk):
+    fornecedor = get_object_or_404(Fornecedor, pk=pk)
+    if request.method == "POST":
+        fornecedor.delete()
+        messages.success(request, "Fornecedor excluído com sucesso!")
+        return redirect('listar_fornecedores')
+
+    return render(request, 'confirmar_exclusao.html', {'fornecedor': fornecedor})
+
+
